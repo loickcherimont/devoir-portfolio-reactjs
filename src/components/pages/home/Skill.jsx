8cb0e978bfee905a8bfd779a1b989298e3463e04
loickcherimont <loickcherimont@gmail.com>
2026-03-13 10:20:55 +0100

    refactor: for homepage and fix some bugfixes

--- src/components/pages/home/Skill.jsx
+++ src/components/pages/home/Skill.jsx
@@ -1,11 +1,11 @@
 function Skill({ color, progressValue, children }) {
     return (
-        <>
+        <div>
             <div>{children} {progressValue}%</div>
             <div className="progress" role="progressbar" aria-label={`Progress in ${children}`} aria-valuenow={progressValue} aria-valuemin="0" aria-valuemax="100">
                 <div className={`progress-bar bg-${color}`} style={{ width: `${progressValue}%` }}></div>
             </div>
-        </>
+        </div>
     );
 }
 
